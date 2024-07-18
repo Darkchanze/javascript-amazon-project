5c3a847d2b9e5cbf60a9beeda36918dbfab30383
Darkchanze <nico.wittemann@t-online.de>
2024-07-18 12:26:55 +0200

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -55,6 +55,7 @@ products.forEach((product) => {
 });
 
 document.querySelector(".js-product-grid").innerHTML = productsHTML;
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
@@ -75,6 +76,13 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         quantity: 1,
       });
     }
-    console.log(cart);
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
